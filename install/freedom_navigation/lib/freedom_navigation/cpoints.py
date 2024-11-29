33288d6bdfb7966b88cc6104ad6a6f6e6ec4b32c
jardeldyonisio <jardel.dyonisio@hotmail.com>
2024-11-29 16:40:53 -0300

    :coffin: removing lognav codes

--- install/freedom_navigation/lib/freedom_navigation/cpoints.py
+++
@@ -1,7 +0,0 @@
-from ktangdt import ktangdt
-from ctpts import ctpts
-
-def cpoints(x):
-    P, ang, dt = ktangdt(x)  # Separate vector x
-    C = ctpts(P, ang, dt)  # Compute the control points
-    return C
--- install/freedom_navigation/lib/freedom_navigation/knots.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def knots(Q, k):
-    P = np.empty((2, 0))
-    P = np.hstack((P, Q[:, k - 1]))
-    return P
