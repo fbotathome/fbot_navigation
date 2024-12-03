e3f6de91761863191e27d6237a1972ed5ff60cf8
Luís Milczarek <luis.milczarek@hotmail.com>
2024-12-03 16:07:43 -0300

    added launch for hardware controllers

---
+++ launch/start_controllers.launch.py
@@ -0,0 +1,94 @@
+from launch import LaunchDescription
+import os
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description() -> None:
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_rviz",
+            default_value="false",
+            description="Start RViz2 automatically with this launch file.",
+        )
+    )
+
+    use_rviz = LaunchConfiguration("use_rviz")
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("fbot_navigation"), "rviz", "navigation.rviz"]
+    )
+
+    rviz_node = Node(
+       package="rviz2",
+       executable="rviz2",
+       name="rviz2",
+       output="log",
+       arguments=["-d", rviz_config_file],
+       condition=IfCondition(use_rviz),
+    )
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("boris_description"),
+            "config",
+            "boris_controllers.yaml",
+        ]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="both",
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+            ("/diffbot_base_controller/cmd_vel", "/cmd_vel"),
+        ],
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        output="both",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["hoverboard_base_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+    nodes = [
+        control_node,
+        joint_state_publisher_node,
+        joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        rviz_node,
+    ]
+
+    return LaunchDescription(declared_arguments + nodes)
