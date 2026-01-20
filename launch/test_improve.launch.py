a3a26e599a271c5c89c985f00a3a4818dfe34d36
kriskappel <kristofer.kappel@gmail.com>
2026-01-20 15:56:03 -0300

    map change and slam select working

--- launch/test_improve.launch.py
+++ launch/test_improve.launch.py
@@ -19,16 +19,16 @@ def generate_launch_description():
 #         }.items()
 #     )
 
-# Padrão com mapa diferente
-    navigation = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory("fbot_navigation"), 'launch', 'improve_navigation.launch.py')
+# # Padrão com mapa diferente
+#     navigation = IncludeLaunchDescription(
+#         PythonLaunchDescriptionSource(
+#             os.path.join(get_package_share_directory("fbot_navigation"), 'launch', 'improve_navigation.launch.py')
         
-        ),
-        launch_arguments={
-            'map_file': 'lab_map_v2.yaml'
-        }.items()
-    )
+#         ),
+#         launch_arguments={
+#             'map_file':'lab_map_v2.yaml'
+#         }.items()
+#     )
 
 # # Padrão com yaml diferente
 #     navigation = IncludeLaunchDescription(
@@ -37,8 +37,8 @@ def generate_launch_description():
         
 #         ),
 #         launch_arguments={
-#             'map_file_arg': 'lab_map_v2.yaml',
-#             'nav_params_file_arg': 'nav2_params_keepout.yaml'
+#             'map_file': 'lab_map_v2.yaml',
+#             'nav_params_file': 'nav2_params_keepout.yaml'
 #         }.items()
 #     )
 
@@ -51,7 +51,7 @@ def generate_launch_description():
 #         launch_arguments={
 #             'use_slam':'true'
 #         }.items()
-#     )
+    # )
 
 # # Slam com improve_slam_toolbox
 #     navigation = IncludeLaunchDescription(
@@ -65,18 +65,18 @@ def generate_launch_description():
 #         }.items()
 #     )
 
-# # Slam com câmera
-#     navigation = IncludeLaunchDescription(
-#         PythonLaunchDescriptionSource(
-#             os.path.join(get_package_share_directory("fbot_navigation"), 'launch', 'improve_navigation.launch.py')
+# Slam com câmera
+    navigation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("fbot_navigation"), 'launch', 'improve_navigation.launch.py')
         
-#         ),
-#         launch_arguments={
-#             'use_slam':'true',
-#             'nav_params_file_arg': 'slam_camera_params.yaml',
-#             'slam_params_file':'improve_slam_toolbox.yaml'
-#         }.items()
-#     )
+        ),
+        launch_arguments={
+            'use_slam':'true',
+            'nav_params_file': 'slam_camera_params.yaml',
+            'slam_params_file':'improve_slam_toolbox.yaml'
+        }.items()
+    )
 
     world = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
